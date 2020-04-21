53164b7b71908639cb55440f9c486b3600d04641
Prudhvi Vempati <11300173@in.techwave.net>
2020-04-21 11:33:28 +0530

    heroku deployment errors fixed

--- src/app.js
+++ src/app.js
@@ -7,44 +7,45 @@ const forecast = require('./utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000
 
-// Define  paths for Express config
-const publicDirectory = path.join(__dirname, '../public')
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-// Setup handlesbars engine and view location 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-//setup static directory to serve
-app.use(express.static(publicDirectory))
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'karthikprudhvi'
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About',
-        name: 'karthikprudhvi'
+        title: 'About Me',
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
+        helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'karthikprudhvi'
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
-            error: 'provide address'
+            error: 'You must provide an address!'
         })
     }
 
@@ -54,9 +55,8 @@ app.get('/weather', (req, res) => {
         }
 
         forecast(latitude, longitude, (error, forecastData) => {
-
             if (error) {
-                return console.log(error)
+                return res.send({ error })
             }
 
             res.send({
@@ -69,15 +69,13 @@ app.get('/weather', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-
     if (!req.query.search) {
         return res.send({
-            error: 'provide search term'
+            error: 'You must provide a search term'
         })
     }
 
-    console.log(req.query.search);
-
+    console.log(req.query.search)
     res.send({
         products: []
     })
@@ -86,16 +84,16 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'karthik',
-        errorMessage: 'help article not found'
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
     })
 })
 
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'karthik',
-        errorMessage: 'Page not found'
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
     })
 })
 
